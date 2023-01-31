15bfe8d200579571990c3db9c9f8f96f3760974f
Or Brener <obrener@uoguelph.ca>
2023-01-31 14:18:33 -0500

    setup inputJumble and it's call in solvejumble

--- solvejumble.f95
+++ solvejumble.f95
@@ -1,13 +1,31 @@
 
+! gfortran -Wall lexicon.f95 solvejumble.f95 && ./a.out
 
-program test                                                                                                                                                                                                                                    
-    integer :: a=5, b=2                                                                                                     
-    integer :: c                                                                                                                                                                                                                                    
-    !                                                                                                                       
-    !this is a test program                                                                                          
-    !                                                                                                                                                                                                                                               
-    write (*,*) 'This is a test'                                                                                            
-    write (*,*) 'What is the value of c: '                                                                                 
-    read (*,*) c                                                                                                            
-    write (*,*) 'c = ',c                                                                                                                                                                                                                            
-end
+subroutine inputJumble(jumbleWord)
+    implicit none
+    !
+    character (len=8), intent(inout) :: jumbleWord
+    write(*,*) 'Enter the jumbled word: '
+    read(*,*) jumbleWord
+
+    return
+
+end subroutine inputJumble
+
+program solvejumble
+    !
+    !
+    !
+
+    integer :: numWords
+    character (len=8) :: jumbleWord
+
+    write(*,*) 'How many jumbled words? '
+    read(*,*) numWords
+
+    do index = 1, numWords
+        call inputJumble(jumbleWord)
+        write(*,*) jumbleWord
+    end do
+
+end program solvejumble
