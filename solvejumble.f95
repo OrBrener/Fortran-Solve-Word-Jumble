53a9588a3497f4ba30076b3fe446b2d8280620b2
Or Brener <obrener@uoguelph.ca>
2023-01-31 17:53:14 -0500

    finished inputJumble

--- solvejumble.f95
+++ solvejumble.f95
@@ -1,31 +1,56 @@
 
 ! gfortran -Wall lexicon.f95 solvejumble.f95 && ./a.out
 
-subroutine inputJumble(jumbleWord)
-    implicit none
-    !
-    character (len=8), intent(inout) :: jumbleWord
-    write(*,*) 'Enter the jumbled word: '
-    read(*,*) jumbleWord
-
-    return
-
-end subroutine inputJumble
-
 program solvejumble
+    ! use lexicon
+    implicit none
+
     !
     !
     !
 
+    integer :: index
     integer :: numWords
-    character (len=8) :: jumbleWord
+    
+    character (len=8), allocatable, dimension(:) :: jumbledWords
 
-    write(*,*) 'How many jumbled words? '
-    read(*,*) numWords
+    call inputJumble(numWords, jumbledWords)
 
     do index = 1, numWords
-        call inputJumble(jumbleWord)
-        write(*,*) jumbleWord
+        write(*,*) jumbledWords(index)
     end do
+
+    contains
+
+    subroutine inputJumble(numWords, jumbledWords)
+        implicit none
+        ! this function takes in numWords number of input strings from the user 
+        ! stores the strings in a dynamically allocated array of strings
+        integer, intent(out) :: numWords
+        character (len=8), allocatable, dimension(:), intent(out) :: jumbledWords
+        integer :: index
+        integer :: inputError = 1
+
+        ! validate input
+        do while (inputError /= 0)
+            write(*,*) 'How many jumbled words? '
+            read(*,*,iostat=inputError) numWords
+            if (inputError /= 0) then
+                write(*,*) 'wrong input, try again'
+            end if
+        end do
+
+        ! allocate memory to the string array
+        allocate(jumbledWords(numWords))
+
+        ! get user string input
+        write(*,*) 'Enter the jumbled word: '
+        do index = 1, numWords
+            read(*,*) jumbledWords(index)
+        end do
+    
+        return
+    
+    end subroutine inputJumble
 
 end program solvejumble
