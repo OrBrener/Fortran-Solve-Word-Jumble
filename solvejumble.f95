43c64389d12957a69de085c8270ed49a112c6f7d
Or Brener <obrener@uoguelph.ca>
2023-01-31 13:55:44 -0500

    add f95 files

---
+++ solvejumble.f95
@@ -0,0 +1,13 @@
+
+
+program test                                                                                                                                                                                                                                    
+    integer :: a=5, b=2                                                                                                     
+    integer :: c                                                                                                                                                                                                                                    
+    !                                                                                                                       
+    !this is a test program                                                                                          
+    !                                                                                                                                                                                                                                               
+    write (*,*) 'This is a test'                                                                                            
+    write (*,*) 'What is the value of c: '                                                                                 
+    read (*,*) c                                                                                                            
+    write (*,*) 'c = ',c                                                                                                                                                                                                                            
+end
